f5182c777cb470797f868836880a3c3ebaee9f0a
me-junaid <jdasjunaid@gmail.com>
2026-01-20 16:27:33 +0530

    Fixing view button bug

--- src/components/HomePage/PromoBanner.jsx
+++ src/components/HomePage/PromoBanner.jsx
@@ -6,7 +6,7 @@ export const PromoBanner = () => {
   const { user } = useContext(ItemsContext)
   
   return (
-    <div className='p-4 m-3 bg-[#1e1e1e] aspect-video rounded-2xl'>
+    <div className='p-4 m-3 bg-[#1e1e1e] h-[150px] rounded-2xl'>
       {user.name}
     </div>
   )
