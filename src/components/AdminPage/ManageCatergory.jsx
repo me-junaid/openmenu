3824fc86c5a80a8c74f19cd7c7fc9f45449acd7e
me-junaid <jdasjunaid@gmail.com>
2026-01-24 13:39:48 +0530

    Navigation bus fixed

--- src/components/AdminPage/ManageCatergory.jsx
+++ src/components/AdminPage/ManageCatergory.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useContext } from "react";
 import { ItemsContext } from "../../contexts/ItemsContext";
 import { AdminContext } from "../../contexts/AdminContext";
+import { Plus } from "../Icons/Plus";
 
 export const ManageCatergory = ({ admin }) => {
 
@@ -12,48 +13,46 @@ export const ManageCatergory = ({ admin }) => {
 
   return (
     <>
-      <h1 className="mt-2.5 text-4xl my-font ml-4 mb-2">Category</h1>
+      <div className="flex items-center justify-between px-4">
+        <h1 className="mt-2.5 text-4xl my-font mb-2">Category</h1>
+        <button className="my-font text-xs mt-2  border border-yellow-100 py-0.5 px-2 rounded-full flex justify-center items-center">
+          <Plus width={25} height={25}/>
+          Add Category</button>
+      </div>
       <div
-        className="sticky top-0"
         role="list"
         aria-label="Category feed"
       >
-        <div className="relative bg-black">
-          <div className=" max-h-[230px] pb-10 px-2 py-1 bg-black grid grid-cols-4 justify-around overflow-x-auto snap-x snap-mandatory hide-scrollbar">
+        <div className="px-2 py-1 bg-black grid grid-cols-4 justify-around overflow-x-auto snap-x snap-mandatory hide-scrollbar">
 
 
-            {items.map((item, index) => {
-              const isSelected = index === selectedCategoryByAdmin;
-              const baseClasses =
-                "p-0.5 flex flex-col justify-center items-center shrink-0 rounded-xl snap-center transition-transform duration-150 min-w-22";
-              const selectedClasses = isSelected
-                ? "scale-100 border-2 border-yellow-200/10"
-                : "scale-100 border-2 border-[#6f6f6f00]";
+          {items.map((item, index) => {
+            const isSelected = index === selectedCategoryByAdmin;
+            const baseClasses =
+              "p-0.5 flex flex-col justify-center items-center shrink-0 rounded-xl snap-center transition-transform duration-150 min-w-22";
+            const selectedClasses = isSelected
+              ? "scale-100 border-2 border-yellow-200/10"
+              : "scale-100 border-2 border-[#6f6f6f00]";
 
-              return (
-                <div
-                  key={index}
-                  role="button"
-                  tabIndex={0}
-                  aria-pressed={isSelected}
-                  onClick={() => {
-                    handleSelectedCategoryByAdmin(index)
-                  }}
-                  onKeyDown={(e) => handleKeyDown(e, index)}
-                  className={`${baseClasses} ${selectedClasses}`}
-                >
-                  <div className="bg-gray-700 w-18 aspect-square rounded-lg" />
-                  <div className={`${isSelected ? "text-yellow-200" : ""} font-bold text-xs my-0.5 text-center my-font`}>{item.name}</div>
-                </div>
-              );
-            })}
-          </div>
-
-          <div
-            className="pointer-events-none absolute -bottom-1 left-0 right-0 h-12
-                 bg-linear-to-t from-black to-transparent z-10"
-          />
+            return (
+              <div
+                key={index}
+                role="button"
+                tabIndex={0}
+                aria-pressed={isSelected}
+                onClick={() => {
+                  handleSelectedCategoryByAdmin(index)
+                }}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+                className={`${baseClasses} ${selectedClasses}`}
+              >
+                <div className="bg-gray-700 w-18 aspect-square rounded-lg" />
+                <div className={`${isSelected ? "text-yellow-200" : ""} font-bold text-xs my-0.5 text-center my-font`}>{item.name}</div>
+              </div>
+            );
+          })}
         </div>
+
 
       </div>
       <h1 className="mt-2.5 text-2xl my-font ml-4 mb-2">Items in <span className="text-yellow-200 my-font animate-pulse-glow">{categoryName}</span></h1>
